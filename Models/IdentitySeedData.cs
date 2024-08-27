72c390461aac917391ae38ce708cef0f6c6b03f0
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-27 11:40:38 +0300

    seed database

---
+++ Models/IdentitySeedData.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models;
+
+public static class IdentitySeedData
+{
+    private const string adminUser = "Admin";
+    
+    private const string adminPassword = "Admin_1234";
+
+    public static async void IdentityTestUser(IApplicationBuilder app)
+    {
+        var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>(); //servis sağlayıcısından IdentityContext sınıfını alıyoruz
+
+        if (context.Database.GetAppliedMigrations().Any())
+        {
+            context.Database.Migrate();
+        }
+        
+        var userManager = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<UserManager<IdentityUser>>(); //servis sağlayıcısından UserManager sınıfını alıyoruz
+
+        var user = await userManager.FindByNameAsync(adminUser); //kullanıcı adına göre kullanıcıyı arıyoruz
+
+        if (user == null)
+        {
+            user = new IdentityUser
+            {
+                UserName = adminUser,
+                Email = "admin@merveeksi.com",
+                PhoneNumber = "555 444 33 22"
+            };
+            
+            await userManager.CreateAsync(user, adminPassword); // kullanıcıyı oluşturuyoruz //bir parola atansın mı diye soruyor //parolayı göremeyiz
+        }
+    }
+}
