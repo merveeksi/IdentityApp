dfdfce999c64569460c2e28299d8037fe48b7458
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-29 15:54:25 +0300

    hgb

--- obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f0bbfa1c5fbff1590186e531014e967c5d11c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00cb9a49f663ac6d44bca97742a8c9f373d43a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
