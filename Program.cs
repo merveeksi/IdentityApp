2f0bbfa1c5fbff1590186e531014e967c5d11c3b
MERVE EKŞİ <merveeksii61@gmail.com>
2024-08-29 15:50:39 +0300

    Identity Options

--- Program.cs
+++ Program.cs
@@ -12,6 +12,19 @@ builder.Services.AddDbContext<IdentityContext>(options =>
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+    
+    options.User.RequireUniqueEmail = true; //email adreslerinin unique olmasını sağlar, başka bir kullanıcı aynı email adresi ile kayıt olamaz
+    options.User.AllowedUserNameCharacters = "abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ0123456789-._"; //kullanıcı adı için geçerli karakterler
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
